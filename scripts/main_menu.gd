19587003c50a5cdbab7dca449723a088c4c35082
amysthat <arduinomacerasi@gmail.com>
2025-02-09 10:42:53 +0300

    remove save button

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,11 +1,12 @@
 extends Control
 
 @onready var continue_button := %Continue
+@onready var remove_save_button := % "Remove Save"
 
 @onready var settings_menu := $Settings
 
 func _ready():
-    continue_button.disabled = not Saving.has_game_save_data()
+    update_continue_and_delete_buttons()
 
 func _on_new_game_pressed():
     if Saving.has_game_save_data():
@@ -21,3 +22,11 @@ func _on_quit_pressed():
 
 func _on_settings_pressed():
     settings_menu.visible = true
+
+func _on_remove_save_pressed():
+    Saving.remove_game_save_data()
+    update_continue_and_delete_buttons()
+
+func update_continue_and_delete_buttons():
+    continue_button.disabled = not Saving.has_game_save_data()
+    remove_save_button.visible = Saving.has_game_save_data()
