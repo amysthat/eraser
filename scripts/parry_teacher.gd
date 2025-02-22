b5f32fb890ec6f3b7cb52ef50ac5505b187b634a
amysthat <arduinomacerasi@gmail.com>
2025-02-22 17:05:58 +0300

    map: canon parry teacher

---
+++ scripts/parry_teacher.gd
@@ -0,0 +1,20 @@
+@tool
+extends Node2D
+
+@export var height: float
+
+func _process(_delta):
+    if Engine.is_editor_hint():
+        queue_redraw()
+        return
+
+    var localized_player_position = Player.instance.global_position - global_position
+
+    for child in get_children():
+        var kid = child as Node2D
+        kid.position = Vector2(0, clamp(localized_player_position.y, -height / 2, height / 2))
+
+func _draw():
+    var vector_size = Vector2.UP * height
+
+    draw_line(-vector_size / 2, vector_size / 2, Color.RED)
