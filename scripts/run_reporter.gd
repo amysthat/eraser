57bcf38ca36e466f10e01bd48ce0a37613a0fc70
amysthat <arduinomacerasi@gmail.com>
2025-04-12 17:07:47 +0300

    time: run info panel

---
+++ scripts/run_reporter.gd
@@ -0,0 +1,7 @@
+extends PanelContainer
+
+@export var time_label: Label
+
+func _process(_delta: float) -> void:
+    time_label.text = "Time: %s" % TimeManager.convert_time_into_legibile_time(TimeManager.elapsed_time)
+	
